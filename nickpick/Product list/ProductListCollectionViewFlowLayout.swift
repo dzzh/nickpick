632ee077ee739d0c28bf8c701e9d5251ca68e856
Zmicier Zaleznicenka <Zmicier.Zaleznicenka@tomtom.com>
2016-11-23 23:22:15 +0100

    collection view works

--- nickpick/Product list/ProductListCollectionViewFlowLayout.swift
+++ nickpick/Product list/ProductListCollectionViewFlowLayout.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ProductListCollectionViewFlowLayout: UICollectionViewFlowLayout {
 
-    let cellMargin: CGFloat = 5.0
+    let cellMargin: CGFloat = 10.0
     let imagePadding: CGFloat = 5.0
     let labelDistance: CGFloat = 10.0
     let titleHeight: CGFloat = 38.0
@@ -18,7 +18,7 @@ class ProductListCollectionViewFlowLayout: UICollectionViewFlowLayout {
 
     var cellSize: CGSize { get {
         let screenWidth = UIScreen.main.bounds.size.width
-        let width: CGFloat = (screenWidth - 3 * cellMargin) / 3
+        let width: CGFloat = (screenWidth - 3 * cellMargin) / 2
         let height: CGFloat = width - imagePadding + labelDistance + titleHeight +
                 labelDistance + priceHeight + labelDistance
         return CGSize(width: width, height: height)
@@ -27,9 +27,8 @@ class ProductListCollectionViewFlowLayout: UICollectionViewFlowLayout {
     func configure() {
         minimumLineSpacing = cellMargin
         minimumInteritemSpacing = cellMargin
-        sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        sectionInset = UIEdgeInsets(top: cellMargin, left: 0, bottom: cellMargin, right: 0)
         itemSize = cellSize
-        estimatedItemSize = cellSize
     }
 
 }

3a218bf3794441ef442bb8f4462747d2639e208a
Zmicier Zaleznicenka <Zmicier.Zaleznicenka@tomtom.com>
2016-11-23 18:54:59 +0100

    created basic project structure

---
+++ Podfile
@@ -0,0 +1,11 @@
+inhibit_all_warnings!
+use_frameworks!
+
+platform :ios, :deployment_target => "10.0"
+
+target 'nickpick' do
+    pod 'ReactiveSwift', :git => 'https://github.com/ReactiveCocoa/ReactiveSwift.git'
+    pod 'Alamofire', '~> 4.2.0'
+    pod 'Kingfisher', '~> 3.2.1'
+end
+
---
+++ nickpick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nickpick
+//
+//  Created by Zmicier Zaleznicenka on 23/11/16.
+//  Copyright © 2016 Zmicier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
